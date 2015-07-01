0edd9cb4fdcdc17b4bb953dd40a022f042d74de0
mlg3672 <michele.goe@gmail.com>
2015-07-01 17:02:47 +0800

    updated image of social network map

--- dorm.py
+++ dorm.py
@@ -20,38 +20,38 @@ prefs=[('Toby', ('Bacchus', 'Hercules')),
 domain=[(0,(len(dorms)*2)-i-1) for i in range(0,len(dorms)*2)]
 
 def printsolution(vec):
-  slots=[]
-  # Create two slots for each dorm
-  for i in range(len(dorms)): slots+=[i,i]
+    slots=[]
+    # Create two slots for each dorm
+    for i in range(len(dorms)): slots+=[i,i]
 
-  # Loop over each students assignment
-  for i in range(len(vec)):
-    x=int(vec[i])
+    # Loop over each students assignment
+    for i in range(len(vec)):
+        x=int(vec[i])
 
-    # Choose the slot from the remaining ones
-    dorm=dorms[slots[x]]
-    # Show the student and assigned dorm
-    print prefs[i][0],dorm
-    # Remove this slot
-    del slots[x]
+        # Choose the slot from the remaining ones
+        dorm=dorms[slots[x]]
+        # Show the student and assigned dorm
+        print(prefs[i][0],dorm)
+        # Remove this slot
+        del slots[x]
 
 def dormcost(vec):
-  cost=0
-  # Create list a of slots
-  slots=[0,0,1,1,2,2,3,3,4,4]
+    cost=0
+    # Create list a of slots
+    slots=[0,0,1,1,2,2,3,3,4,4]
 
-  # Loop over each student
-  for i in range(len(vec)):
-    x=int(vec[i])
-    dorm=dorms[slots[x]]
-    pref=prefs[i][1]
-    # First choice costs 0, second choice costs 1
-    if pref[0]==dorm: cost+=0
-    elif pref[1]==dorm: cost+=1
-    else: cost+=3
-    # Not on the list costs 3
+    # Loop over each student
+    for i in range(len(vec)):
+        x=int(vec[i])
+        dorm=dorms[slots[x]]
+        pref=prefs[i][1]
+        # First choice costs 0, second choice costs 1
+        if pref[0]==dorm: cost+=0
+        elif pref[1]==dorm: cost+=1
+        else: cost+=3
+        # Not on the list costs 3
 
-    # Remove selected slot
-    del slots[x]
+        # Remove selected slot
+        del slots[x]
     
-  return cost
+    return cost
